941319d146f1602daa968d7ad97a7c3f1e7f2f5c
Daniil-Tsiunchyk <fcad.td@gmail.com>
2024-04-03 21:53:18 +0300

    init

---
+++ riga/src/test/java/com/belarus/riga/RigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belarus.riga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
