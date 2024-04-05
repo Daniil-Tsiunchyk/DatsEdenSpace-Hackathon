da80743c0ec472c6a6e7d23354e37073aa380e41
Daniil-Tsiunchyk <fcad.td@gmail.com>
2024-04-05 20:51:53 +0300

    feat: add script PlanetCounter and devendencies for GraphVisualizator

---
+++ riga/src/main/java/com/belarus/riga/scripts/PlanetCounter.java
@@ -0,0 +1,37 @@
+package com.belarus.riga.scripts;
+
+import com.belarus.riga.classes.PlayerUniverseResponse;
+import com.belarus.riga.client.UniverseClient;
+
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class PlanetCounter {
+
+    public static void main(String[] args) {
+        UniverseClient client = new UniverseClient();
+        try {
+            TreeSet<String> uniquePlanets = fetchUniquePlanets(client);
+            printUniquePlanets(uniquePlanets);
+        } catch (Exception e) {
+            System.err.println("An error occurred while fetching and processing universe data: " + e.getMessage());
+        }
+    }
+
+    private static TreeSet<String> fetchUniquePlanets(UniverseClient client) throws Exception {
+        PlayerUniverseResponse response = client.getPlayerUniverse("660e963e5bc03660e963e5bc06");
+        return response.getUniverse().stream()
+                .flatMap(planetInfo -> planetInfo.stream().limit(2))
+                .map(Object::toString)
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    private static void printUniquePlanets(TreeSet<String> uniquePlanets) {
+        System.out.println("Total unique planets: " + uniquePlanets.size());
+        System.out.println("Sorted and numbered list of unique planets:");
+        int count = 1;
+        for (String planet : uniquePlanets) {
+            System.out.println(count++ + ". " + planet);
+        }
+    }
+}
